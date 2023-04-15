51d083d340fcf1c089413f192d770cf3f06e0d79
Bastien <bastien.jean.perso@gmail.com>
2023-04-15 20:01:49 +0200

    feat(hue-slider): Update hue slider style

--- src/colorPicker/components/HueSlider.vue
+++ src/colorPicker/components/HueSlider.vue
@@ -19,9 +19,22 @@ defineEmits(['update:modelValue'])
 </template>
 
 <style scoped lang="scss">
-.clr-pckr-hue-select {
+.clr-pckr-hue-slider {
+  appearance: none;
   width: 100%;
-  // background-image: linear-gradient(to right,red 0,#ff0 16.66%,#0f0 33.33%,#0ff 50%,#00f 66.66%,#f0f 83.33%,red 100%);
-  background-image: linear-gradient(to right,hsl(319, 96%, 49%) 0,#ff0 16.66%,#0f0 33.33%,#0ff 50%,#00f 66.66%,#f0f 83.33%,red 100%);
+  height: 6px;
+  border-radius: 999px;
+  background-image: linear-gradient(to right,red 0,#ff0 16.66%,#0f0 33.33%,#0ff 50%,#00f 66.66%,#f0f 83.33%,red 100%);
+
+  &::-webkit-slider-thumb {
+    appearance: none;
+    background-color: Hsl(v-bind(modelValue), 100%, 50%);
+    border-radius: 999px;
+    width: 20px;
+    height: 20px;
+    border: 3px solid white;
+    cursor: pointer;
+    box-shadow: 0 0 5px hsl(0, 0%, 50%);
+  }
 }
 </style>
