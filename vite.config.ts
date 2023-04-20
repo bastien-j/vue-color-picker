3d2b6136be90ec77a23b0fbbb29db3101ddbca39
Bastien <bastien.jean.perso@gmail.com>
2023-04-20 12:51:01 +0200

    build(): Fix build and type generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
 import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/colorPicker/index.ts"),
+      name: "VueColorPicker",
+      // the proper extensions will be added
+      fileName: "vue-color-picker",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
